cca988585cfa7f9b525a22f44c80b45892501869
Limbureuben <limbureuben4@gmail.com>
2025-02-16 17:06:57 +0300

    changes

--- openspace/settings.py
+++ openspace/settings.py
@@ -146,6 +146,16 @@ REST_FRAMEWORK = {
     ),
 }
 
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+    "DEFAULT_PERMISSION_CLASSES": (
+        "rest_framework.permissions.IsAuthenticated",
+    ),
+}
+
+
 CORS_ALLOW_ALL_ORIGINS = True #allow all origin
 
 # for speficic  origin
