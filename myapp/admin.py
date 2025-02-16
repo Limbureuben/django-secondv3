0c5e50980882d262e471b35cc065a78afd568151
Limbureuben <limbureuben4@gmail.com>
2025-02-16 17:28:03 +0300

    changes

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Registration)
+class RegistrationAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email']
+    search_fields = ['name', 'email']
+
+admin.site.register(Login)
+class LoginAdmin(admin.ModelAdmin):
+    list_display = ['username']
+    search_fields = ['username']
