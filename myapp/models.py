98716395db3e69e5ee4d735e239e7a77f852684a
Limbureuben <limbureuben4@gmail.com>
2025-02-17 12:51:36 +0300

    changes

--- myapp/models.py
+++ myapp/models.py
@@ -5,6 +5,7 @@ class Registration(models.Model):
     name = models.CharField(max_length=200)
     email= models.EmailField(max_length=200)
     password = models.CharField(max_length=200)
+    passwordConfirm = models.CharField(max_length=200)
 
 class Login(models.Model):
     username = models.CharField(max_length=200)
