6da4f3bc93935a8d8f14eba75ec72c59c7623cd1
Limbureuben <limbureuben4@gmail.com>
2025-02-17 12:42:12 +0300

    changes

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+import graphene
 from openspace.openspaceBuilders import *
 from openspace.openspace_dto import *
+
+
+class RegistrationMutation(graphene.Mutation):
+    message = graphene.String()
+
+    class Arguments:
+        input =  RegistrationInput(required=True)
