8fa8a1b37d6c748ba948a07cbf83eb863ef6d4c0
Limbureuben <limbureuben4@gmail.com>
2025-02-17 14:15:43 +0300

    changes

--- openspaceBuilders/openspaceBuilders.py
+++ openspaceBuilders/openspaceBuilders.py
@@ -1,10 +1,11 @@
 from openspace.openspace_dto.Response import *
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, UserProfile
 from django.core.exceptions import ValidationError
 import uuid
 from django.conf import settings
 from django.http import HttpResponse
 from myapp.models import *
+from myapp.tasks import send_verification_email
 
 # class register_user:
 #     def __init__(self, input):
@@ -27,8 +28,29 @@ class UserBuilder:
         if len(password) < 8:
             raise ValidationError("Password must be at least 8 characters long")
         
-        user = User.objects.create(username=username, email=email)
+        if user.objects.filter(username=username).exists():
+            raise ValidationError("Username already taken")
+        
+        if User.objects.filter(email=email).exists():
+            raise ValidationError("Email already taken")
+        
+        user = User(username=username, email=email)
         user.set_password(password)
+        user.is_superuser = False
+        user.is_staff = False
         user.save()
 
-        return RegistrationResponse(message="User registred successfully", success=True)
+        user_profile = UserProfile(user=user, verification_token=uuid.uuid4())
+        user_profile.save()
+
+        verification_url = f"{settings.BACKEND_URL}/verify-email/{user_profile.verification_token}/"
+        send_verification_email.delay(email, verification_url)
+        return user
+    
+def register_user(input):
+    try:
+        user = UserBuilder.register_user(input.username, input.email, input.password, input.passwordConfirm)
+        return RegistrationResponse(message="Registration successful. Please check your email to verify your account", success=True)
+    except ValidationError as e:
+        return RegistrationResponse(message=str(e), success=False)
+    
