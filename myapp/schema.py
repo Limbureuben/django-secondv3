d7a116bc0eb257bbf996dc7c8a10f272d7f7c439
Limbureuben <limbureuben4@gmail.com>
2025-02-18 16:47:07 +0300

    changes

--- myapp/schema.py
+++ myapp/schema.py
@@ -1,16 +1,13 @@
 import graphene
 from .models import *
 from django.contrib.auth.models import User
-
 from openspaceBuilders.openspaceBuilders import *
 from openspace_dto.openspace import *
 from .views import *
 import graphql_jwt
 
-
 class Mutation(graphene.ObjectType):
     register_users = RegistrationMutation.Field()
-
 
 class Query(graphene.ObjectType):
     pass
