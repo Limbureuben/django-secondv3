6fa3be7627599b9b55bcd6d47a6a556a455541ac
Limbureuben <limbureuben4@gmail.com>
2025-02-25 17:19:38 +0300

    chnages

--- openspaceBuilders/openspaceBuilders.py
+++ openspaceBuilders/openspaceBuilders.py
@@ -59,7 +59,8 @@ class UserBuilder:
         # Generate JWT tokens
         refresh = RefreshToken.for_user(user)
         return {
-           
+            "user": user,
+            "refresh_token": str(refresh),
             "access_token": str(refresh.access_token),
         }
 
