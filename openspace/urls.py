262318bcd8136e038d595803b5c73ffef3f6ab7f
Limbureuben <limbureuben4@gmail.com>
2025-02-28 13:06:52 +0300

    the chnages

--- openspace/urls.py
+++ openspace/urls.py
@@ -19,8 +19,11 @@ from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
 from django.urls import path, include
 
+from myapp.views import verify_email
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
     path('', include('myapp.urls')),
+    path('verify-email/<uuid:token>/', verify_email, name='verify_email'),
 ]
