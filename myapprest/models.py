8d3048bbb8fee450be0eac537f41b4388dbb3ea3
Limbureuben <limbureuben4@gmail.com>
2025-05-04 23:41:35 +0300

    changes

--- myapprest/models.py
+++ myapprest/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class UssdReport(models.Model):
@@ -10,3 +11,4 @@ class UssdReport(models.Model):
 
     def __str__(self):
         return self.reference_number
+
